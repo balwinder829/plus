88e0fa05aeced42cd2310f66c136a3d1c00c4c9a
nihalsinghrajput8 <nihalsingh.rajput8@gmail.com>
2022-02-12 15:50:30 +0530

    created login and register api

---
+++ app/Models/PlusUser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class PlusUser extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    protected $guarded = ['id'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'full_name', 'email', 'password','mobile','rooms','smscode','smsCodeExpriration','insetTime','lastupdate','status','taxon_status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ database/migrations/2022_02_12_075730_create_plus_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlusUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plus_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->bigInteger('mobile');
+            $table->integer('rooms')->nullable();
+            $table->string('smscode')->nullable();
+            $table->timestamp('smsCodeExpriration')->nullable();
+            $table->timestamp('insetTime')->nullable();
+            $table->timestamp('lastupdate')->nullable();
+            $table->integer('status')->nullable();
+            $table->integer('taxon_status')->nullable();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plus_users');
+    }
+}
